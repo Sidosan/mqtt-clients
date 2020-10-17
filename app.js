d65da8358f31feca646a74a334f83502af792f91
Sidosan <filipe.sidosan@gmail.com>
2020-10-17 01:02:12 -0300

    conectando com o broker

--- app.js
+++ app.js
@@ -1,8 +1,29 @@
 const { MongoClient } = require('mongodb');
+const mqtt = require('mqtt');
 
 const mongo = require('mongodb').MongoClient;
 MongoClient.connect("mongodb://root:admin@localhost:27017/?authSource=admin")
-        .then(conn => console.log(conn))
+        .then(conn => console.log("Conexao realizada com sucesso!"))
         .catch(err => console.log(err));
 
+var options = {
+    port: 1883,
+    host: "localhost",
+    clientId: 'ClientNode',
+    username: 'usenode',
+    password: 'node',
+    keeplive: 60
+
+};
+
+var client = mqtt.connect("mqtt://localhost", options);
+client.on('connect', function(){
+    console.log("Acesso liberado ao Broker!")
+
+    client.subscribe('+/#', function(){
+
+        console.log("Inscrito ao tropico!");
+    })
+
+});
 
