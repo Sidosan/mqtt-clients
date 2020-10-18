3ed99c9ba4bb3b8f94fd194bf966375e535fae71
Sidosan <filipe.sidosan@gmail.com>
2020-10-18 20:06:04 -0300

    commit mongo modern

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
-const { MongoClient } = require('mongodb');
-const mqtt = require('mqtt');
+const mqtt=require('mqtt')  
+const mongodb=require('mongodb'); 
+var mongodbClient = mongodb.MongoClient;  
+var mongodbURI = 'mongodb://root:admin@localhost:27017/admin        .....tgggew'
 
 const mongo = require('mongodb').MongoClient;
 MongoClient.connect("mongodb://root:admin@localhost:27017/?authSource=admin")
         .then(conn => console.log("Conexao realizada com sucesso!"))
         .catch(err => console.log(err));
-
 var options = {
     port: 1883,
     host: "localhost",
@@ -13,17 +14,15 @@ var options = {
     username: 'usenode',
     password: 'node',
     keeplive: 60
-
 };
-
 var client = mqtt.connect("mqtt://localhost", options);
 client.on('connect', function(){
     console.log("Acesso liberado ao Broker!")
-
-    client.subscribe('+/#', function(){
-
-        console.log("Inscrito ao tropico!");
+    client.subscribe('+/#', function(err){
+        console.log("Inscrito ao topico!");
     })
-
 });
 
+client.on('message', function(topic, message){
+    console.log("Topico "+ topic + "\nMensagem "+ message.toString());
+});
